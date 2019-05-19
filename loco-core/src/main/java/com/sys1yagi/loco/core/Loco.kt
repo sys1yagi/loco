fef94c2707b7482e57bcfa7912203f44c4261a3a
Toshihiro Yagi <sylc.yagi@gmail.com>
2019-05-19 12:58:56 +0900

    Introduce LocoRunner.

--- loco-core/src/main/java/com/sys1yagi/loco/core/Loco.kt
+++ loco-core/src/main/java/com/sys1yagi/loco/core/Loco.kt
@@ -1,130 +1,34 @@
 package com.sys1yagi.loco.core
 
-import com.sys1yagi.loco.core.internal.SmashedLog
-import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.consumeEach
-import kotlin.reflect.KClass
+import com.sys1yagi.loco.core.internal.LocoRunner
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
 
 object Loco {
-
-    sealed class Event {
-        data class Store(val log: SmashedLog) : Event()
-        data class Send(val config: LocoConfig) : Event()
-    }
-
-    private var config: LocoConfig? = null
-
-    private var channel: Channel<Event>? = null
-
-    private var mainJob: Job? = null
-
-    private var waitForNextSendingJob: Job? = null
+    private var runner: LocoRunner? = null
 
     fun start(
         config: LocoConfig,
         coroutineScope: CoroutineScope = GlobalScope
     ) {
         requireNotInitialized()
-
-        Loco.config = config
-        channel = Channel(Channel.UNLIMITED)
-        mainJob = coroutineScope.launch {
-            channel?.consumeEach { event ->
-                when (event) {
-                    is Event.Store -> {
-                        config.store.store(event.log)
-                    }
-                    is Event.Send -> {
-                        send(coroutineScope, event.config)
-                    }
-                }
-            }
+        runner = LocoRunner(config).also {
+            it.start(coroutineScope)
         }
-        channel?.offer(Loco.Event.Send(config))
     }
 
     fun stop() {
-        config = null
-        mainJob?.cancel()
-        mainJob = null
+        runner?.stop()
+        runner = null
     }
 
     fun send(log: LocoLog) {
-        val config = requireInitialized()
-        val smasher = config.smasher
-        val senderType = findSenderTypeWithLocoLog(log, config)
-        val smashed = smasher.smash(log)
-        val smashedLog = SmashedLog(
-            log::class.java.name,
-            smasher::class.java.name,
-            senderType.java.name,
-            smashed
-        )
-        channel?.offer(Loco.Event.Store(smashedLog))
+        requireInitialized()
+        runner?.send(log)
     }
 
-    private suspend fun send(scope: CoroutineScope, config: LocoConfig) {
-        val logs = config.store.load(config.sendingBulkSize)
-        logs.groupBy { it.senderTypeName }.forEach { entry ->
-            val senderTypeName = entry.key
-            val senderTypedLogs = entry.value
-            val sender = findSender(senderTypeName, config)
-
-            when (sender.send(senderTypedLogs)) {
-                SendingResult.SUCCESS, SendingResult.FAILED -> {
-                    config.store.delete(senderTypedLogs)
-                }
-                SendingResult.RETRY -> {
-                    // no op
-                }
-            }
-        }
-
-        waitForNextSendingJob?.cancel()
-        waitForNextSendingJob = scope.launch {
-            // TODO delay config
-            delay(5000)
-            channel?.offer(Loco.Event.Send(config))
-        }
-    }
-
-    private fun findSender(senderTypeName: String, config: LocoConfig): Sender {
-        val senderType = Class.forName(senderTypeName)::kotlin.get()
-        return config.senders.find {
-            it::class.java == senderType.java
-        } ?: throw IllegalStateException(
-            """
-                    Missing Smasher.
-                    Add ${senderType.java.name} to LocoConfig#smashers.
-                    """.trimIndent()
-        )
-    }
-
-    private fun findSenderTypeWithLocoLog(log: LocoLog, config: LocoConfig): KClass<out Sender> {
-        return config.mapping.logToSender.entries.find { map ->
-            map.value.any { it == log::class }
-        }?.key ?: throw IllegalStateException(
-            """
-                    Missing mapping to Sender Type.
-                    Set up the mapping of ${log::class.java.name} class.
-                    """.trimIndent()
-        )
-    }
-
-    private fun findSenderTypeWithKClass(klass: KClass<out LocoLog>, config: LocoConfig): KClass<out Sender> {
-        return config.mapping.logToSender.entries.find { map ->
-            map.value.any { it == klass }
-        }?.key ?: throw IllegalStateException(
-            """
-                    Missing mapping to Sender Type.
-                    Set up the mapping of ${klass::class.java.name} class.
-                    """.trimIndent()
-        )
-    }
-
-    private fun requireInitialized(): LocoConfig {
-        return config ?: throw IllegalStateException(
+    private fun requireInitialized() {
+        runner ?: throw IllegalStateException(
             """
                     Loco is not yet initialized.
                     Please call start(config: LocoConfig) first.
@@ -133,7 +37,7 @@ object Loco {
     }
 
     private fun requireNotInitialized() {
-        if (this.config != null) {
+        if (this.runner != null) {
             throw IllegalStateException(
                 """
                     Loco is already initialized.
---
+++ loco-core/src/main/java/com/sys1yagi/loco/core/internal/LocoRunner.kt
@@ -0,0 +1,131 @@
+package com.sys1yagi.loco.core.internal
+
+import com.sys1yagi.loco.core.LocoConfig
+import com.sys1yagi.loco.core.LocoLog
+import com.sys1yagi.loco.core.Sender
+import com.sys1yagi.loco.core.SendingResult
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.consumeEach
+import kotlin.reflect.KClass
+
+class LocoRunner(val config: LocoConfig) {
+    sealed class Event {
+        data class Store(val log: SmashedLog) : Event()
+        data class Send(val config: LocoConfig) : Event()
+    }
+
+    private var channel: Channel<Event>? = null
+
+    private var mainJob: Job? = null
+
+    private var waitForNextSendingJob: Job? = null
+
+    fun start(
+        coroutineScope: CoroutineScope = GlobalScope
+    ) {
+        requireNotInitialized()
+
+        channel = Channel(Channel.UNLIMITED)
+        mainJob = coroutineScope.launch {
+            channel?.consumeEach { event ->
+                when (event) {
+                    is Event.Store -> {
+                        config.store.store(event.log)
+                    }
+                    is Event.Send -> {
+                        sending(coroutineScope, event.config)
+                    }
+                }
+            }
+        }
+        channel?.offer(Event.Send(config))
+    }
+
+    fun stop() {
+        mainJob?.cancel()
+        mainJob = null
+    }
+
+    fun send(log: LocoLog) {
+        val smasher = config.smasher
+        val senderType = findSenderTypeWithLocoLog(log, config)
+        val smashed = smasher.smash(log)
+        val smashedLog = SmashedLog(
+            log::class.java.name,
+            smasher::class.java.name,
+            senderType.java.name,
+            smashed
+        )
+        channel?.offer(Event.Store(smashedLog))
+    }
+
+    private suspend fun sending(scope: CoroutineScope, config: LocoConfig) {
+        val logs = config.store.load(config.sendingBulkSize)
+        logs.groupBy { it.senderTypeName }.forEach { entry ->
+            val senderTypeName = entry.key
+            val senderTypedLogs = entry.value
+            val sender = findSender(senderTypeName, config)
+
+            when (sender.send(senderTypedLogs)) {
+                SendingResult.SUCCESS, SendingResult.FAILED -> {
+                    config.store.delete(senderTypedLogs)
+                }
+                SendingResult.RETRY -> {
+                    // no op
+                }
+            }
+        }
+
+        waitForNextSendingJob?.cancel()
+        waitForNextSendingJob = scope.launch {
+            // TODO delay config
+            delay(5000)
+            channel?.offer(Event.Send(config))
+        }
+    }
+
+    private fun findSender(senderTypeName: String, config: LocoConfig): Sender {
+        val senderType = Class.forName(senderTypeName)::kotlin.get()
+        return config.senders.find {
+            it::class.java == senderType.java
+        } ?: throw IllegalStateException(
+            """
+                    Missing Sender.
+                    Add ${senderType.java.name} to LocoConfig#senders.
+                    """.trimIndent()
+        )
+    }
+
+    private fun findSenderTypeWithLocoLog(log: LocoLog, config: LocoConfig): KClass<out Sender> {
+        return config.mapping.logToSender.entries.find { map ->
+            map.value.any { it == log::class }
+        }?.key ?: throw IllegalStateException(
+            """
+                    Missing mapping to Sender Type.
+                    Set up the mapping of ${log::class.java.name} class.
+                    """.trimIndent()
+        )
+    }
+
+    private fun findSenderTypeWithKClass(klass: KClass<out LocoLog>, config: LocoConfig): KClass<out Sender> {
+        return config.mapping.logToSender.entries.find { map ->
+            map.value.any { it == klass }
+        }?.key ?: throw IllegalStateException(
+            """
+                    Missing mapping to Sender Type.
+                    Set up the mapping of ${klass::class.java.name} class.
+                    """.trimIndent()
+        )
+    }
+
+    private fun requireNotInitialized() {
+        if (channel != null) {
+            throw IllegalStateException(
+                """
+                    LocoRunner is already started.
+                    """.trimIndent()
+            )
+        }
+    }
+}
