ea70e28af6106c3dfddac890b3f14454247316c0
Toshihiro Yagi <sylc.yagi@gmail.com>
2019-05-19 09:36:55 +0900

    Add loco-store-android-sqlite module. not yet implement.

---
+++ loco-store-android-sqlite/src/main/java/com/sys1yagi/loco/store/android/sqlite/LocoAndroidSqliteStore.kt
@@ -0,0 +1,18 @@
+package com.sys1yagi.loco.store.android.sqlite
+
+import com.sys1yagi.loco.core.Store
+import com.sys1yagi.loco.core.internal.SmashedLog
+
+class LocoAndroidSqliteStore : Store{
+    override suspend fun store(log: SmashedLog) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun load(size: Int): List<SmashedLog> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun delete(logs: List<SmashedLog>) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
