0912acfa5f061e15885597d44c47f016ec077129
Toshihiro Yagi <sylc.yagi@gmail.com>
2019-05-19 09:22:05 +0900

    migrate tot androidx.

--- sample/src/main/java/com/sys1yagi/loco/sample/MainActivity.kt
+++ sample/src/main/java/com/sys1yagi/loco/sample/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.sys1yagi.loco.sample
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.google.gson.Gson
